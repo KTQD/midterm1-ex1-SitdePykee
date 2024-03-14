7c5ae06b9274229c6abe9fe0f6c59645be35688b
SitdePykee <129248580+SitdePykee@users.noreply.github.com>
2024-03-14 07:48:07 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        MP3 mp3;
+        MP3 mp3 = new MP3("Máy nghe nhạc Samsung 3000", "Tiện lợi nhỏ gọn", "001", 23, 3);
         mp3.showInfo();
 
-        Book book;
+        Book book = new Book("Con cừu vàng", "Sách văn học kinh điển kinh dị", "012", 99, "Conan Hercules", 64, "Sách văn học");
         book.showInfo();
 
     }
