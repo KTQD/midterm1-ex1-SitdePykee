4e897d606fd0e2933e355c0e248ef46538a0172b
SitdePykee <129248580+SitdePykee@users.noreply.github.com>
2024-03-14 07:47:33 +0700

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,22 @@
-public class Item {
+public abstract class Item {
+    public String name;
+    private String description;
+    protected String ID;
+    int price;
+    public Item(String name, String description, String ID, int price){
+        this.name = name;
+        this.description = description;
+        this.ID = ID;
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public abstract void showInfo();
 }
